e9f7ad31c2af8b2d9829d84dad7c9ac1d0530280
Alexandra <alexandra.chavez.gallardo@outlook.es>
2025-04-01 14:09:07 -0500

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/group05/Group05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
