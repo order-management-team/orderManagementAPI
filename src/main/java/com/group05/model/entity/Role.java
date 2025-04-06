9d36af35f082990fc1ede3d8224704960af6aa37
Alexandra <alexandra.chavez.gallardo@outlook.es>
2025-04-06 02:37:55 -0500

    refactor: JPA relation entities and project structure

--- src/main/java/com/group05/model/entity/Role.java
+++ src/main/java/com/group05/model/entity/Role.java
@@ -1,28 +1,25 @@
 package com.group05.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name="roles")
+@Entity
+@Table(name="roles", schema = "order_mgmt")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
     @Column(unique = true)
     private String name;
+
     @Column
     private String description;
 
-    @OneToMany(mappedBy = "role")
-    @JsonManagedReference(value = "role-users")
-    private List<User> users = new ArrayList<>();
 }
--- src/main/java/com/group05/model/entity/User.java
+++ src/main/java/com/group05/model/entity/User.java
@@ -1,11 +1,9 @@
 package com.group05.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
@@ -13,31 +11,35 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name="users")
+@Entity
+@Table(name = "users", schema="order_mgmt")
 public class User {
-    @jakarta.persistence.Id
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
     @Column
     private String name;
+
     @Email(message = "El formato de correo no es válido")
     @NotBlank(message = "El correo no puede estar vacío")
     @Column(unique = true)
     private String email;
+
     @NotBlank
     @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
-    @Pattern(regexp = ".*[0-9].*", message = "La contraseña debe contener al menos un número")
-    @Pattern(regexp = ".*[!@#$%^&*()].*", message = "La contraseña debe contener al menos un símbolo especial")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,}$", message = "La contraseña debe tener al menos 8 caracteres, un número y un símbolo especial")
     @Column
     private String password;
+
     @Column
     private LocalDate signUpDate;
+
     @Column
     private Double totalSpent;
 
     @ManyToOne
     @JoinColumn(name = "role_id")
-    @JsonBackReference(value = "user-role")
     private Role role;
 }
